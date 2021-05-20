eeb9c1482b7f1d17ae3dbb4a9abbcf3d07e5023d
Leonardo Arthur <lraguiar.la@gmail.com>
2021-05-19 22:29:22 -0300

    resolução de bugs ex014

--- Aula012ex/ex014/script.js
+++ Aula012ex/ex014/script.js
@@ -1,20 +1,20 @@
 function carregar() {
-var msg = document.getElementById('msg')
-var img = document.getElementById("imagem")
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById("imagem")
 var data = new Date()
 var hora = data.getHours()
 msg.innerHTML = `Agora são ${hora} horas.`
 if (hora >= 0 && hora < 12){
     //Bom Dia!
-    img.scr = "manhã.png"
+    img.scr = 'manha.png'
     document.body.style.background = '#de934f'
-} else if (hora >= 12 && hora < 18) {
+} else if (hora >= 12 && hora <= 18) {
     //Boa Tarde!
-    img.scr = "tarde.img"
+    img.scr = 'tarde.png'
     document.body.style.background = '#aaa58f'
 }else {
     //Boa Noite!
-    img.scr = "noite.img"
+    img.scr = 'noite.png'
     document.body.style.background = '#19181b'
 }
 
